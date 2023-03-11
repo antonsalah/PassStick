49c8d78c097e654a9b747058a00fad1130281513
KristopherKohlmeier <Kristopherkohlmeier@gmail.com>
2023-03-11 02:40:52 -0800

    integrated room api for SQLlite integration and created the initial database via room

--- app/src/main/java/com/example/passtickmain/DBController.kt
+++ app/src/main/java/com/example/passtickmain/DBController.kt
@@ -1,2 +1,15 @@
 package com.example.passtickmain
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "accounts")
+
+
+
+data class account(
+    @PrimaryKey(autoGenerate = true)
+    var serviceName: String,
+    var userName: String,
+    var password: String
+)
