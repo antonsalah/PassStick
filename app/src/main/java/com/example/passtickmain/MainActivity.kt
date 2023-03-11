8ce35944ba255775c587eef2a97ce4a1c579177e
Harrison Power <harrison.power224@myci.csuci.edu>
2023-03-10 21:41:53 -0800

    Implemented Basic UI, Input, and Password List Reading

--- app/src/main/java/com/example/passtickmain/MainActivity.kt
+++ app/src/main/java/com/example/passtickmain/MainActivity.kt
@@ -3,26 +3,105 @@ package com.example.passtickmain
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.passtickmain.ui.theme.PasstickMainTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import com.example.passtickmain.ui.theme.Purple200
+import com.example.passtickmain.ui.theme.Purple500
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import com.example.passtickmain.ui.theme.Teal200
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.material.Typography
+import com.example.passtickmain.ui.theme.Purple700
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import android.widget.Toast
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.platform.LocalContext
 
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
+
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+)
+
+//Values of color palates
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+//Creates UI Theme
+@Composable
+fun PasstickUITheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PasstickMainTheme {
+            PasstickUITheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column {
+                        AddMenu()
+                        Spacer(modifier = Modifier.padding(10.dp))
+                        DisplayPasswordList(SampleData.passwordListSample)
+                    }
                 }
             }
         }
@@ -30,14 +109,75 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AddMenu() {
+    val usernameInput = rememberSaveable { mutableStateOf("Username") }
+    val passwordInput = rememberSaveable { mutableStateOf("Password") }
+    val ctx = LocalContext.current
+
+    TextField (
+        value = usernameInput.value,
+        onValueChange = { newValue ->
+            usernameInput.value = newValue }
+    )
+    Spacer(modifier = Modifier.padding(10.dp))
+    TextField (
+        value = passwordInput.value,
+        onValueChange = { newValue ->
+            passwordInput.value = newValue }
+    )
+    Spacer(modifier = Modifier.padding(10.dp))
+    Row {
+        Button(
+            onClick = {
+                Toast.makeText(ctx, "Added ${usernameInput.value} ${passwordInput.value}", Toast.LENGTH_LONG).show()
+            }) {
+            Text(text = "Add")
+        }
+        Spacer(modifier = Modifier.padding(5.dp))
+        Button(
+            onClick = {
+
+            }) {
+            Text(text = "Initialize")
+        }
+    }
+}
+@Composable
+fun AddButton() {
+    val ctx = LocalContext.current
+    val text = "some text"
+    Button(onClick = {
+        Toast.makeText(ctx, text, Toast.LENGTH_LONG).show()
+    }) {
+        Text(text = text)
+    }
+}
+data class AccountEntry(val username: String, val password: String)
+
+//Given a list of AccountEntry, display each given the
+@Composable
+fun DisplayPasswordList(entries: List<AccountEntry>) {
+    LazyColumn {
+        items(entries) { entry ->
+            DisplayEntry(entry)
+        }
+    }
 }
 
+//Displays one entry given an AccountEntry
+@Composable
+fun DisplayEntry(entry: AccountEntry) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Text(text = entry.username + "   ")
+        Text(text = entry.password)
+    }
+}
+
+//Preview function for DisplayPasswordList
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    PasstickMainTheme {
-        Greeting("Android")
+fun PreviewDisplayPasswordList() {
+    PasstickUITheme {
+        DisplayPasswordList(entries = SampleData.passwordListSample)
     }
 }
