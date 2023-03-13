7f654ef8337458ef2058796ae6adab88fe370290
antonsalah <anton.salah511@myci.csuci.edu>
2023-03-13 12:49:55 -0700

    Merge remote-tracking branch 'origin/develop' into Feature_Anton

--- app/src/main/java/com/example/passtickmain/data/AccountDao.kt
+++ app/src/main/java/com/example/passtickmain/data/AccountDao.kt
@@ -8,16 +8,16 @@ import kotlinx.coroutines.flow.Flow
 interface AccountDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(account: Account)
+    fun insert(account: Account)
 
     @Update
-    suspend fun  update(account: Account)
+    fun  update(account: Account)
 
     @Delete
-    suspend fun delete(account: Account)
+    fun delete(account: Account)
 
     @Query("SELECT * FROM accounts")
-    fun getAllAccounts(): Flow<List<Account>>
+    fun getAllAccounts(): <List<Account>>
 
     @Query("SELECT * FROM accounts WHERE id = accountId")
     fun getAccount(accountId: Int): Flow<Account>
