77ac8e1b1cff53373f8a20828aac4ab44190d8e9
KristopherKohlmeier <Kristopherkohlmeier@gmail.com>
2023-04-02 18:16:15 -0700

    ViewModel for home activity done for now

--- app/src/main/java/com/example/passtickmain/ui/viewmodels/HomeActivityViewmodel.kt
+++ app/src/main/java/com/example/passtickmain/ui/viewmodels/HomeActivityViewmodel.kt
@@ -6,24 +6,22 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewModelScope
 
 import com.example.passtickmain.data.*
+import kotlinx.coroutines.flow.*
 
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.toList
 
 
 //wip
 class HomeActivityViewmodel(private val accountDao: AccountDao): ViewModel() {
-    var fullAccountList: StateFlow<List<Account>> = accountDao.getAllAccounts()
-        .stateIn(
-            initialValue = Loading,
-            scope = viewModelScope,
-            started = WhileSubscribed(5000)
 
-        )
-
-    var singleAccount: StateFlow<Account> = accountDao.getAccount()
+    var fullAccountList: StateFlow<FullAccountList> =
+        accountDao.getAllAccounts().map { FullAccountList(it) }
+            .stateIn(
+                scope = viewModelScope,
+                started = WhileSubscribed(5000),
+                initialValue = FullAccountList()
+            )
 
 }
+
+data class FullAccountList(val itemList: List<Account> = listOf())
