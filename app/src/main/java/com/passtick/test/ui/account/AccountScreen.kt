ad0827afdc55a1ee18a36af404c5b96d0dbc5284
jordan206 <jordan.schaefer206@myci.csuci.edu>
2023-04-09 18:26:34 -0700

    add floating action button

--- app/src/main/java/com/passtick/test/ui/account/AccountScreen.kt
+++ app/src/main/java/com/passtick/test/ui/account/AccountScreen.kt
@@ -16,28 +16,33 @@
 
 package com.passtick.test.ui.account
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Color.CYAN
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle.State.STARTED
 import androidx.lifecycle.repeatOnLifecycle
-import com.passtick.test.ui.theme.MyApplicationTheme
 import com.passtick.test.data.local.database.Account
-import android.widget.Toast
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import com.passtick.test.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -141,8 +146,32 @@ internal fun AccountScreen(
             }
         }
         PasswordListDisplay(accountList = accountList, state = listState, onDelete, onModify)
+
+        ScaffoldSample()
     }
 }
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldSample(){
+
+    Scaffold(
+        topBar = { },
+        bottomBar = { },
+        floatingActionButtonPosition = FabPosition.Center
+    ) {
+
+        //
+        FloatingActionButton(
+            onClick = { /* do something */ },
+        ) {
+            Icon(Icons.Filled.Add, "Localized description")
+        }
+    }
+
+}
+
 
 @Composable
 fun AccountDisplay(account: Account, onDelete: (account: Account) -> Unit, onModify: (account: Account) -> Unit) {
