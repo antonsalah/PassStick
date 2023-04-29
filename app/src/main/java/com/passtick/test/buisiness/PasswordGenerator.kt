21e59fea15b9f9d00b2167c923e4168476d90b11
KristopherKohlmeier <Kristopherkohlmeier@gmail.com>
2023-04-29 16:08:09 -0700

    update branch

---
+++ app/src/main/java/com/passtick/test/buisiness/PasswordGenerator.kt
@@ -0,0 +1,16 @@
+package com.passtick.test.buisiness
+
+import java.security.SecureRandom
+
+class PasswordGenerator {
+
+    fun generatePassowrd(Length : Int = 12, includedChars : List<Char>){
+
+        var defaultCharList : MutableList<Char>
+
+        for(char in 'a' .. 'z')
+            defaultCharList.add(char)
+
+
+    }
+}
