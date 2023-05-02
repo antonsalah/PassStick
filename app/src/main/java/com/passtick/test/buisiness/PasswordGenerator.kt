5d065b820b810e2c28f86ffb7021cca2b9fff2ed
KristopherKohlmeier <kristopherkohlmeier@gmail.com>
2023-05-02 16:52:32 -0700

    Finished Passsword Generation Algorithm

--- app/src/main/java/com/passtick/test/buisiness/PasswordGenerator.kt
+++ app/src/main/java/com/passtick/test/buisiness/PasswordGenerator.kt
@@ -1,16 +1,53 @@
 package com.passtick.test.buisiness
 
 import java.security.SecureRandom
+import kotlin.random.Random
+
 
 class PasswordGenerator {
 
-    fun generatePassowrd(Length : Int = 12, includedChars : List<Char>){
+    fun generatePassowrd(Length : Int = 12, includeNumbers: Boolean, includeSymbols: Boolean) : String{
 
-        var defaultCharList : MutableList<Char>
+        val charList = mutableListOf<Char>()
+        val tempPass = mutableListOf<Char>()
+        val symbols = listOf('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '-', '"', ':', ';', '[', ']', '\\', '\'', '|', ',', '<', '>', '.', '?')
+        var password : String = ""
+        var size = Length
+        var random = Random.nextInt(charList.size)
+
 
         for(char in 'a' .. 'z')
-            defaultCharList.add(char)
+            charList.add(char)
+
+        for(char in 'A' .. 'Z')
+            charList.add(char)
 
 
+
+        if(includeNumbers) {
+            for (char in '0'..'9') {
+                charList.add(char)
+            }
+            tempPass.add(Random.nextInt(10).toChar())
+            size--
+        }
+
+        if(includeSymbols){
+            for (char in symbols){
+                charList.add(char)
+            }
+           tempPass.add(symbols[Random.nextInt(symbols.size)])
+            size--
+        }
+
+
+        repeat(size){
+           tempPass.add(charList[Random.nextInt(charList.size)])
+        }
+
+        tempPass.shuffle()
+        password = tempPass.toString()
+
+        return password
     }
 }
